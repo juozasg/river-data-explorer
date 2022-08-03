654f4f002df3bc283904e5b0e0554ae1775b82bf
Juozas <juozasgaigalas@gmail.com>
2022-08-03 17:25:26 +0300

    datatable show selected sites

--- src/components/DataTable.svelte
+++ src/components/DataTable.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { fly } from 'svelte/transition';
 
-  import { viewportHeight, showDataSelector, showTimeseries, showDataTable } from '../lib/stores';
+  import { viewportHeight, showDataSelector, showTimeseries, showDataTable, selectedSites } from '../lib/stores';
 
   let width = 392;
   let height = 200;
@@ -19,7 +19,17 @@
   <div id='data-table' class='elevation'
         transition:fly="{{ x: -200, duration: 400 }}"
         style='width: {width}px; height: {height}px'>
-    <h2>DATA-TABLE</h2>
+    <div class='content'>
+
+      <h2>Data Table</h2>
+      <h3>{$selectedSites.size} site(s) selected</h3>
+      <ul>
+        {#each Array.from($selectedSites) as site}
+        <li>{site}</li>
+        {/each}
+
+      </ul>
+    </div>
   </div>
 {/if}
 
@@ -32,5 +42,9 @@
     height: 100px;
     background-color: white;
     z-index: 2;
+
+    .content {
+      padding: 0 0.5rem 0.5rem 1rem;
+    }
   }
 </style>
