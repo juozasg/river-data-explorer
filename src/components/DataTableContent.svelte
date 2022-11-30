4c16dee7d715f15731a117131a99f71adecab68e
Juozas G <juozasgaigalas@gmail.com>
2022-11-30 20:15:06 +0200

    datatable wip

--- src/components/DataTableContent.svelte
+++ src/components/DataTableContent.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { fly, slide } from 'svelte/transition';
+  import DataTable, { Head, Body, Row, Cell } from '@smui/data-table';
   import List, { Item, Separator, Text, PrimaryText, SecondaryText } from '@smui/list';
   import Tab, { Label } from '@smui/tab';
   import TabBar from '@smui/tab-bar';
@@ -48,7 +49,7 @@
     get(mapStore).setView([site.lat, site.lon + offset], 12);;
   }
 
-  let active = 'Selected';
+  let active = 'All';
 </script>
 
 
@@ -60,28 +61,94 @@
       <Label>{tab}</Label>
     </Tab>
   </TabBar>
-  <h3>{$selectedSites.size} site(s) selected</h3>
-  <h3>{labels[$selectedSeries]}:</h3>
-  <List threeLine nonInteractive >
+
+  {#if active === 'Selected'}
+    <h3>{$selectedSites.size} site(s) selected</h3>
+    <h3>{labels[$selectedSeries]}:</h3>
+    <List threeLine nonInteractive >
+      {#each Array.from($selectedSites).reverse() as siteId}
+      <div transition:slide>
+        <Item>
+          <Text>
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            <PrimaryText><a class="pointer" on:click={() => centerMapOn(siteId)}>{siteId}</a></PrimaryText>
+            <SecondaryText>{getSite(siteId).name}</SecondaryText>
+            {#if $selectedSeries === 'datainfo' || valueUndefined(siteId, selectedSeries)}
+              <SecondaryText><b>{valueWithUnits(siteId, $selectedSeries)}</b></SecondaryText>
+            {:else}
+              <SecondaryText>{valueDate(siteId, $selectedSeries)}: <b>{valueWithUnits(siteId, $selectedSeries)}</b></SecondaryText>
+            {/if}
+          </Text>
+        </Item>
+      </div>
+      {/each}
+
+    </List>
+  {/if}
+
+  {#if active === 'All'}
     {#each Array.from($selectedSites).reverse() as siteId}
-    <div transition:slide>
-      <Item>
-        <Text>
-          <!-- svelte-ignore a11y-missing-attribute -->
-          <!-- svelte-ignore a11y-click-events-have-key-events -->
-          <PrimaryText><a class="pointer" on:click={() => centerMapOn(siteId)}>{siteId}</a></PrimaryText>
-          <SecondaryText>{getSite(siteId).name}</SecondaryText>
-          {#if $selectedSeries === 'datainfo' || valueUndefined(siteId, selectedSeries)}
-            <SecondaryText><b>{valueWithUnits(siteId, $selectedSeries)}</b></SecondaryText>
-          {:else}
-            <SecondaryText>{valueDate(siteId, $selectedSeries)}: <b>{valueWithUnits(siteId, $selectedSeries)}</b></SecondaryText>
+      <div transition:slide>
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <PrimaryText><a class="pointer" on:click={() => centerMapOn(siteId)}>{siteId}</a></PrimaryText>
+        <SecondaryText>{getSite(siteId).name}</SecondaryText>
+
+        {#each Object.keys(labels) as k}
+          {#if k === 'datainfo'}
+            <SecondaryText><b>{valueWithUnits(siteId, k)}</b></SecondaryText>
+          {:else if !valueUndefined(siteId, k)}
+            <SecondaryText>{labels[k]}: <b>{valueWithUnits(siteId, k)}</b></SecondaryText>
           {/if}
-        </Text>
-      </Item>
-    </div>
+        {/each}
+      </div>
+      <hr/>
+
     {/each}
 
-  </List>
+  {/if}
+
+
+  {#if active === 'Statistics'}
+
+  <DataTable table$aria-label="People list" style="max-width: 100%;">
+    <Head>
+      <Row>
+        <Cell>Name</Cell>
+        <Cell>Favorite Color</Cell>
+        <Cell numeric>Favorite Number</Cell>
+      </Row>
+    </Head>
+    <Body>
+      <Row>
+        <Cell>Steve</Cell>
+        <Cell>Red</Cell>
+        <Cell numeric>45</Cell>
+      </Row>
+      <Row>
+        <Cell>Sharon</Cell>
+        <Cell>Purple</Cell>
+        <Cell numeric>5</Cell>
+      </Row>
+      <Row>
+        <Cell>Rodney</Cell>
+        <Cell>Orange</Cell>
+        <Cell numeric>32</Cell>
+      </Row>
+      <Row>
+        <Cell>Mack</Cell>
+        <Cell>Blue</Cell>
+        <Cell numeric>12</Cell>
+      </Row>
+    </Body>
+  </DataTable>
+   
+
+  {/if}
+
+
+  
 </div>
 
 <style lang="scss">
