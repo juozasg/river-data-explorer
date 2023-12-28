d86fed9d7775a0acaa5e0b4d8e029a0709854026
Juozas Gaigalas <juozasgaigalas@gmail.com>
2023-12-29 00:39:52 +0200

    remove reference to svelte4

--- svelte4/src/lib/stores/viewport.ts
+++
@@ -1,9 +0,0 @@
-import { writable, get } from 'svelte/store';
-
-export const viewportWidth = writable(window.innerWidth);
-export const viewportHeight = writable(window.innerHeight);
-
-window.addEventListener('resize', () => {
-  viewportWidth.set(window.innerWidth);
-  viewportHeight.set(window.innerHeight);
-});
