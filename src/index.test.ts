18cc2c897a598d2f86a5a9151cb37d535fd7f3ac
Juozas Gaigalas <juozasgaigalas@gmail.com>
2023-12-29 02:21:45 +0200

    unit test WIP

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,25 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
+import { createRoot } from 'svelte';
+
+import IndexPage from '$routes/+page.svelte'
 
 describe('sum test', () => {
 	it('adds 1 + 2 to equal 3', () => {
 		expect(1 + 2).toBe(3);
 	});
+
+	let instance = null;
+
+	beforeEach(() => {
+		const host = document.createElement('div');
+		document.body.appendChild(host);
+
+		instance = createRoot(IndexPage, {target: host});
+		instance.$set({foo: 'WIP'});
+		console.log(instance);
+		// TODO: https://github.com/testing-library/svelte-testing-library/issues/284
+		// instance = render()
+	});
+
+
 });
