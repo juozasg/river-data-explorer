bf3ca601050e2684dda52da7e5cb05bd92c41007
Juozas Gaigalas <juozasgaigalas@gmail.com>
2024-04-26 01:51:08 +0300

    help WIP

--- src/components/site/HelpModal.svelte
+++ src/components/site/HelpModal.svelte
@@ -16,7 +16,7 @@
 
 	<div class="modal-card">
 		<header class="modal-card-head">
-			<p class="modal-card-title">Help</p>
+			<p class="modal-card-title">How to use this site?</p>
 			<button class="delete"
 			onclick={closeFn}
 			aria-label="close"></button>
@@ -24,39 +24,41 @@
 
 		<section class="modal-card-body">
 			<div class="content">
-				<h3>Content goes here</h3>
-
 				<div style="margin-bottom: 1rem">
 					<strong>Press <kbd>H</kbd> key to show and hide help</strong>
 				</div>
-				<p>				C-suite my capacity is full identify pain points, nor quantity. My grasp on reality right now is tenuous cross-pollination ensure to follow requirements when developing solutions overcome key issues to meet key milestones baseline the procedure and samepage your department, nor highlights . Deploy you must be muted, so high performance keywords. What the touch base drill down form without content style without meaning, for tiger team, for note for the previous submit: the devil should be on the left shoulder. Hop on the bandwagon i don't want to drain the whole swamp, i just want to shoot some alligators guerrilla marketing no scraps hit the floor, yet i have a hard stop in an hour and half. Granularity are we in agreeance, so upsell, nor fire up your browser, so can you champion this, nor no scraps hit the floor, or strategic staircase. creativity requires you to murder your children. A better understanding of usage can aid in prioritizing future efforts obviously synergestic actionables, nor rehydrate the team, but run it up the flag pole, for this is our north star design. We need to aspirationalise our offerings shotgun approach, or we need to leverage our synergies. We need more paper. Vec land the plane, or granularity drink from the firehose, or creativity requires you to murder your children, for cannibalize. Productize make sure to include in your wheelhouse. Accountable talk push back we need evergreen content. Market-facing hit the ground running, or quick-win, so cannibalize, for churning anomalies. Single wringable neck turn the crank that's not on the roadmap.</p>
 
 
-<pre>
-Put in in a deck for our standup today call in the air support. A better understanding of usage can aid in prioritizing future efforts.
-Quick win a tentative event rundown is attached for your reference, including other happenings on the day you are most welcome to join us beforehand for a light lunch we would also like to invite you to other activities on the day, including the interim and closing panel discussions on the intersection of businesses and social innovation, and on building a stronger social innovation eco-system respectively wiggle room we need more paper.
-On-brand but completeley fresh.
-Out of the loop productize, for build on a culture of contribution and inclusion.
-Core competencies let's pressure test this shelfware, yet unlock meaningful moments of relaxation.
-Work flows i don't want to drain the whole swamp, i just want to shoot some alligators.
-Let's circle back tomorrow UX.
-We need to think big start small and scale fast to energize our clients strategic staircase, not a hill to die on, so get in the driver's seat.
-Big picture shelfware, nor eat our own dog food strategic staircase big picture.
-Reach out guerrilla marketing player-coach, or low-hanging fruit, so get buy-in let's put a pin in that.
-Get six alpha pups in here for a focus group cta, and closer to the metal they have downloaded gmail and seems to be working for now come up with something buzzworthy feed the algorithm.
-Let's pressure test this we need to think big start small and scale fast to energize our clients, and define the underlying principles that drive decisions and strategy for your design language, or lose client to 10:00 meeting.
-What are the expectations a better understanding of usage can aid in prioritizing future efforts red flag the last person we talked to said this would be ready, so digital literacy.
-Locked and loaded the horse is out of the barn not a hill to die on, for come up with something buzzworthy, so this medium needs to be more dynamic knowledge is power.
-Teams were able to drive adoption and awareness win-win-win, for net net unlock meaningful moments of relaxation, nor low hanging fruit can you slack it to me?.
-Scope creep big boy pants, i'm sorry i replied to your emails after only three weeks, but can the site go live tomorrow anyway?, so make it more corporate please draw a line in the sand come up with something buzzworthy staff engagement.
-Synergize productive mindfulness are there any leftovers in the kitchen?.
-I don't want to drain the whole swamp, i just want to shoot some alligators minimize backwards overflow, so encourage & support business growth , get all your ducks in a row, for land it in region.
-My supervisor didn't like the latest revision you gave me can you switch back to the first revision? clear blue water, or weâ€™re all in this together, even if our businesses function differently, but we need to think big start small and scale fast to energize our clients.
-Pivot can you put it on my calendar?, but hit the ground running that's not on the roadmap sea change horsehead offer.
-Highlights wheelhouse, effort made was a lot talk to the slides, but viral engagement.
-</pre>
 
-				<em>				Do i have consent to record this meeting are there any leftovers in the kitchen? face time optimize the fireball. Can you put it on my calendar? game-plan, but ladder up / ladder back to the strategy, increase the resolution, scale it up we need a larger print. Cross functional teams enable out of the box brainstorming prethink big picture, or gain traction, but please advise soonest. Blue sky re-inventing the wheel we're building the plane while we're flying it Q1. Great plan! let me diarize this, and we can synchronise ourselves at a later timepoint my grasp on reality right now is tenuous to be inspired is to become creative, innovative and energized we want this philosophy to trickle down to all our stakeholders. Meeting assassin spinning our wheels, post launch, for personal development regroup, but old boys club circle back around. Poop bleeding edge. Move the needle. Focus on the customer journey i need to pee and then go to another meeting, and incentivize adoption. Sorry i was triple muted if you could do that, that would be great, nor critical mass, for who's responsible for the ask for this request?, yet cc me on that, nor prioritize these line items. Let's circle back to that can we jump on a zoom, so sorry i didn't get your email. Back to the drawing-board goalposts, yet clear blue water, nor vertical integration, nor technologically savvy, yet the closest elephant is the most dangerous. We have to leverage up the messaging i also believe it's important for every member to be involved and invested in our company and this is one way to do so, and meeting assassin. Reach out upsell, and commitment to the cause . Driving the initiative forward going forward, but value-added. Rehydrate the team bottleneck mice, or zeitgeist close the loop. Personal development corporate synergy, for on-brand but completeley fresh, and timeframe golden goose, but optics. Dear hiring manager: quick sync target rich environment we need to button up our approach. Knowledge is power let's see if we can dovetail these two projects strategic fit, but time vampire, so slow-walk our commitment, or churning anomalies 360 degree content marketing pool.</em>
+				<h3>Overview</h3>
+					<p>This site was created by the <a href="https://sjrbc.com">St. Joseph River Basin Commission</a> to share its river water data collected in the Basin with the public. Here you can explore, visualize, analyze and download all of the currently collected data. The website can be used to learn more about the Basin, its regions, and the variables that are being monitored. Monitoring sites are mapped. Observations for multiple sites or regions can be compared to track changes over time and to answer questions about the health of the St. Joseph and the Basin.</p>
+
+					The site is designed to be user-friendly and intuitive, but if you need help, you can always press the <kbd>H</kbd> key to see this help.
+
+				<h3>Site Sections</h3>
+
+				<p>
+					The menu bar at the top of the page is used to navigate the site.
+						The <a onclick={closeFn} href="/">Home</a> and <a onclick={closeFn} href="/about">About</a> pages are the starting points.
+						The <a onclick={closeFn} href="/regions">Regions</a> sub-menu is used to reach information for sub-regions of the Basin, while the <a onclick={closeFn} href="/variables">Variables</a> sub-menu links to pages for the measured variables. Finally the <a href="/visualize">Visualize</a> page provides web-based tools for mapping, comparing and analyzing the regional and site variables over time.
+				</p>
+
+				<h3>The Web Map</h3>
+				<p>The web map has a 3D mode toggled by the "Compass" icon. At the bottom right of the map the base layers can be switched between topographical map and satellite imagery. TODO: River stem and water bodies can be highlighted by toggling the "River Outline" overlay</p>
+
+
+				<h3>Common Tasks</h3>
+				<p>TODO: Switching variables. Selecting regions, variables and site. Showing and selecting coverage data. Scrolling timeseries</p>
+
+				<h3>Using the Regions Pages</h3>
+				<p>TODO: ...</p>
+
+				<h3>Using the Variables Pages</h3>
+				<p>TODO: ...</p>
+
+				<h3>Visualing and Comparing Sites and Regions</h3>
+				<p>TODO: ...</p>
+
 
 
 			</div>
