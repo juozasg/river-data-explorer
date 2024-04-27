beb3268c11b98cad384c4e07ed58a3101474b485
Juozas Gaigalas <juozasgaigalas@gmail.com>
2024-04-28 02:53:58 +0300

    basin menu

---
+++ src/routes/basin/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import WebMap from "$src/components/WebMap.svelte";
+
+
+</script>
+
+<svelte:head>
+	<title>Search the Basin</title>
+</svelte:head>
+
+<h4 class="has-text-centered">Select region or area type</h4>
+
+
+<div class="tabs is-toggle is-centered">
+  <ul>
+    <li class="is-active">
+      <a>
+        <span>State</span>
+      </a>
+    </li>
+    <li>
+      <a>
+        <span>County</span>
+      </a>
+    </li>
+    <li>
+      <a>
+        <span>River (HUC10)</span>
+      </a>
+    </li>
+    <li>
+      <a>
+        <span>Stream (HUC12)</span>
+      </a>
+    </li>
+    <li>
+      <a>
+        <span>Custom Area...</span>
+      </a>
+    </li>
+  </ul>
+</div>
+
+
+<WebMap/>
+
+<style>
+	h4 {
+		margin-bottom: 0.2rem;
+	}
+</style>
