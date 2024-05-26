ae457385e9e04c7c7b570f39dbcf17700999a0dc
Juozas Gaigalas <juozasgaigalas@gmail.com>
2024-05-26 18:50:56 +0300

    package code cleanup

--- package-data.sh
+++ package-data.sh
@@ -7,10 +7,9 @@ curl -L https://github.com/juozasg/SJRBC-web-map-data/archive/refs/heads/webapp.
 unzip data.zip
 mv SJRBC-web-map-data-webapp data
 cd data
-# rm SJRBC-web-map-data.zip
+
+
 find * -type f -exec sha1sum {} \; > sha1.txt
-# cat sha1.txt | jq -nR '{inputs | split("  ") | {sha1: .[0], path: .[1]}}'
-# jq -R 'split(" ") | {(.[0]): .[1]}' sha1.txt
 echo "{" > data-manifest.json
 while IFS='  ' read -r sha name; do
     echo '  "'$name'": "'$sha'",' >> data-manifest.json
