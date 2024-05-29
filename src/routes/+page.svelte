1f32907cf5702ea44bc78dcda63958abae84e322
Juozas Gaigalas <juozasgaigalas@gmail.com>
2024-05-29 03:54:29 +0300

    layers dropdown html wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,35 @@
 
 </script>
 
-
 <svelte:head>
 	<title>St. Joseph River Basin Commission</title>
 	<meta name="description" content="St Joseph River (Indiana) watershed water quality information, data and web map">
+  <link rel="stylesheet" href="https://bulma.io/vendor/fontawesome-free-6.5.2-web/css/all.min.css">
 </svelte:head>
 
 <h1>St. Joseph River Basin Data</h1>
 
+<div class="dropdown">
+  <div class="dropdown-trigger">
+    <button class="button is-small" aria-haspopup="true" aria-controls="dropdown-menu3">
+      <span class="dropdown-label">Layers</span>
+      <span class="dropdown-arrow"></span>
+    </button>
+  </div>
+  <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+    <div class="dropdown-content">
+      <a href="#" class="dropdown-item"> Overview </a>
+      <a href="#" class="dropdown-item"> Modifiers </a>
+      <a href="#" class="dropdown-item"> Grid </a>
+      <a href="#" class="dropdown-item"> Form </a>
+      <a href="#" class="dropdown-item"> Elements </a>
+      <a href="#" class="dropdown-item"> Components </a>
+      <a href="#" class="dropdown-item"> Layout </a>
+      <hr class="dropdown-divider" />
+      <a href="#" class="dropdown-item"> More </a>
+    </div>
+  </div>
+</div>
 
 <AreaSelectorMap zoom={7.8}/>
 
