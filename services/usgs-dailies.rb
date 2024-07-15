b13a14ee6ecc32a3265968fd9fe3bbfcfa9d9399
Juozas Gaigalas <juozasgaigalas@gmail.com>
2024-07-15 20:52:23 +0300

    work more now

--- services/usgs-dailies.rb
+++ services/usgs-dailies.rb
@@ -1,10 +1,13 @@
-today = Time.now.strftime("%F ")
+today = Time.now.strftime("%F")
 
 url = "https://waterservices.usgs.gov/nwis/dv/?format=json&sites=\
 04096405,04096515,04097500,040975299,04097540,04099000,04100500,04101000,04101500,04101535,04101800,04102500,04099750&statCd=00003\
 &siteStatus=all\
-&startDT=2000-01-01
-\&endDT=#{today}"
+&startDT=2000-01-01\
+&endDT=#{today}"
+
+
+puts url
 
 # TODO: get data from url.
 # cache for one day.
