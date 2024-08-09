0b120bd00883b392c026d2425bf883a6693938f4
Juozas Gaigalas <juozasgaigalas@gmail.com>
2024-08-09 18:16:39 +0300

    chart wip

--- src/components/site/debug/TestCharts.svelte
+++ src/components/site/debug/TestCharts.svelte
@@ -5,22 +5,25 @@
 	import { sitesTables } from '$src/appstate/data/datasets.svelte';
 	import type ColumnTable from 'arquero/dist/types/table/column-table';
 	import { onMount } from 'svelte';
-	import ScatterCanvas from '$src/components/chart/layercake/ScatterCanvas.svelte';
+	import ScatterCanvas from '$src/components/chart/layercake/Scatter.svelte';
 	import AxisX from '$src/components/chart/layercake/AxisX.svelte';
 	import AxisY from '$src/components/chart/layercake/AxisY.svelte';
 	import Line from '$src/components/chart/layercake/Line.svelte';
 	import { fmtDate } from '$src/lib/utils';
+	import SharedTooltip from '$src/components/chart/layercake/SharedTooltip.svelte';
+	import Scatter from '$src/components/chart/layercake/Scatter.svelte';
 
-	const table: ColumnTable | undefined = $derived(sitesTables.get('sjrbc-1'));
+	const table: ColumnTable | undefined = $derived(sitesTables.get('sjrbc-1')?.select('date', 'ph'));
+	const points = $derived(table?.objects() || [])
 
-	// Define some data
-	const points = [
-		{ x: 0, y: 0 },
-		{ x: 5, y: 10 },
-		{ x: 10, y: 20 },
-		{ x: 15, y: 30 },
-		{ x: 20, y: 220 }
-	];
+	// // Define some data
+	// const points = [
+	// 	{ x: 0, y: 0 },
+	// 	{ x: 5, y: 10 },
+	// 	{ x: 10, y: 20 },
+	// 	{ x: 15, y: 30 },
+	// 	{ x: 20, y: 220 }
+	// ];
 
 
 
@@ -28,6 +31,8 @@
 		console.log('TestCharts', table);
 		console.log('date 0', table?.object(0))
 	});
+
+
 
 
 	// onMount(() => {
@@ -39,31 +44,49 @@
 		// console.log(d);
 		return fmtDate(date);
 	}
+
+	const color = '#ab00d6';
 </script>
+
+<div id="box">
+	BOX
+</div>
 
 <div id="test">
 	<h2>Hello TestCharts</h2>
 
+
+
 	<div class="chart-container">
 		{#if table}
-		<LayerCake data={table.objects()} x="date" y="ph" >
+		<LayerCake data={points} x="date" y="ph" >
 			<!-- Components go here -->
 			<Svg>
 				<AxisX tickMarks={true} snapLabels={true} ticks={5} format={formatDate} />
 				<AxisY />
-				<Line/>
+				<Line stroke={color}/>
+				<Scatter r={3} fill={color}/>
 			</Svg>
+			<Html>
+				<SharedTooltip formatTitle={formatDate}/>
+			</Html>
 		</LayerCake>
 		{/if}
 	</div>
 </div>
 
 <style>
+	#box {
+		width: 100px;
+		height: 200px;
+		background-color: purple;
+	}
 	.chart-container {
 		width: 400px;
 		height: 300px;
 		border:  1px solid red;
 		margin-left: 2rem;
+		/* position: absolute; */
 		/* background-color: blueviolet; */
 	}
 
@@ -71,8 +94,10 @@
 		width: 100%;
 		height: 800px;
 		border: 1px solid blue;
-		overflow: auto !important;
-		position: relative;
+		overflow: visible;
+		/* position: absolute; */
+		top: 50px;
+		left: 50px;
 	}
 	#test :global(table) {
 		width: 100%;
