7ae092fd6e2ad12fa137a1a01eea1aa1a21dcb25
Juozas Gaigalas <juozasgaigalas@gmail.com>
2024-12-03 18:00:29 +0200

    vanila latest svelte

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    tsconfigPaths(),
+    svelte()],
 })
