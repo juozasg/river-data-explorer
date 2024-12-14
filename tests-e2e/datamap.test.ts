66edf1ac7abd70ceef1cd6d4c30fb1ba3c8c899a
Juozas Gaigalas <juozasgaigalas@gmail.com>
2024-12-14 18:47:25 +0200

    tests run

--- tests-e2e/datamap.test.ts
+++ tests-e2e/datamap.test.ts
@@ -5,13 +5,17 @@ test('has a map with controls', async ({ page }) => {
   await page.goto('/');
 
   // TODO: wait for map load condition/selector
-  await page.waitForTimeout(2000);
-   // to be a canvas element
-  expect(page.getByLabel('Map'), 'has map').toBeVisible();
-  expect(page.getByLabel('Legend'), 'has map legend').toBeVisible();
-  expect(page.getByLabel('Variable Selector'), 'has map variable selector').toBeVisible();
-  expect(page.getByLabel('Data Controls'), 'has map data control').toBeVisible();
-  expect(page.getByLabel('Time Selector'), 'has datetime controls').toBeVisible();
+  // await page.waitForTimeout(3000);
+  // to be a canvas element
+  expect(page.locator('.map').first(), 'has map container').toBeDefined();
+  // await page.getByLabel('Map')
+
+  await expect(page.getByLabel('Map'), 'has map').toBeVisible();
+  await expect(page.getByLabel('Map'), 'map is maplibre canvas').toHaveClass('maplibregl-canvas');
+  await expect(page.getByLabel('Legend'), 'has map legend').toBeVisible();
+  await expect(page.getByLabel('Variable Selector'), 'has map variable selector').toBeVisible();
+  await expect(page.getByLabel('Data Controls'), 'has map data control').toBeVisible();
+  // expect(page.getByLabel('Time Selector'), 'has datetime controls').toBeVisible();
 
   // expect(page.locator('body')).toHaveText(/Hello world x1!/);
 
