d272d3ef38d7e8dc2d50813265fc8e75f5456fd8
Juozas Gaigalas <juozasgaigalas@gmail.com>
2025-01-05 05:43:24 +0200

    rudimentary CI

---
+++ ci.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+set -x
+
+# Vitest
+yarn test
+
+# Start yarn dev in the background and redirect output to a log file
+yarn dev > yarn-dev.log 2>&1 &
+
+# Wait for the server to be up and running
+timeout=5
+while ! curl -s http://localhost:5173 > /dev/null; do
+  timeout=$((timeout - 1))
+  if [ $timeout -le 0 ]; then
+    echo "yarn dev did not start in time"
+    exit 1
+  fi
+  sleep 1
+done
+
+
+# Run Playwright tests
+CI=true yarn run playwright test --fully-parallel --workers 4
