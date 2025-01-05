e8780eae178ad83e9a035d928e48ded8f1984afc
Juozas Gaigalas <juozasgaigalas@gmail.com>
2025-01-05 05:48:53 +0200

    no playwright for now

--- ci.sh
+++ ci.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-set -e
-set -x
+# set -e
+# set -x
 
-# Vitest
-yarn test
+# # Vitest
+# yarn test
 
-# Start yarn dev in the background and redirect output to a log file
-yarn dev > yarn-dev.log 2>&1 &
+# # Start yarn dev in the background and redirect output to a log file
+# yarn dev > yarn-dev.log 2>&1 &
 
-# Wait for the server to be up and running
-timeout=5
-while ! curl -s http://localhost:5173 > /dev/null; do
-  timeout=$((timeout - 1))
-  if [ $timeout -le 0 ]; then
-    echo "yarn dev did not start in time"
-    exit 1
-  fi
-  sleep 1
-done
+# # Wait for the server to be up and running
+# timeout=5
+# while ! curl -s http://localhost:5173 > /dev/null; do
+#   timeout=$((timeout - 1))
+#   if [ $timeout -le 0 ]; then
+#     echo "yarn dev did not start in time"
+#     exit 1
+#   fi
+#   sleep 1
+# done
 
 
 # Run Playwright tests
-yarn playwright install
-CI=true yarn run playwright test --fully-parallel --workers 4
+# yarn playwright install
+# CI=true yarn run playwright test --fully-parallel --workers 4
