320251249f78f178090f1a2cc209cea017d21191
Juozas Gaigalas <juozasgaigalas@gmail.com>
2025-02-16 19:12:34 +0200

    ui2 desktop layout

--- src/components/test-pages/fixtures/UI2-Test.svelte
+++ src/components/test-pages/fixtures/UI2-Test.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import AppMenu from "$src/components/ui2/AppMenu.svelte";
+
 
 
 	const isMobile = false;
@@ -7,13 +9,27 @@
 
 <main>
 	Congrats UI2!
-	<div class="panel panel-map"></div>
-	<div class="panel panel-data1"></div>
-	<div class="panel panel-data2"></div>
-	<div class="panel panel-chart"></div>
+	<div class="app-header">
+		<AppMenu />
+	</div>
+	<div class="panel panel-map">
+		<h1>Map</h1>
+	</div>
+	<div class="panel panel-data1">
+		<h1>Data 1</h1>
+	</div>
+	<div class="panel panel-data2">
+		<h1>Data 2</h1>
+	</div>
+	<div class="panel panel-chart">
+		<h1>Graph</h1>
+	</div>
 </main>
 
 <style>
+	:global(*) {
+		overflow: visible;
+	}
 	main {
 		padding: 0;
 		margin: 0;
@@ -25,33 +41,48 @@
 		/* font-size: 120%; */
 	}
 
+	.app-header {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 50%;
+		height: 36px;
+		background: #ffbcee;
+	}
+
 	.panel {
 		width: 50%;
 		height: 50%;
 		position: absolute;
+		h1 {
+			position: absolute;
+			top: 50%;
+			width: 100%;
+			text-align: center;
+		}
 	}
 
 	.panel-map {
-		top: 0;
+		top: 36px;
 		left: 0;
 		background: rgb(184, 183, 255);
 	}
 
 	.panel-data1 {
-		top: 0;
+		top: 0px;
 		right: 0;
 		background: rgb(183, 255, 184);
 	}
 
 	.panel-data2 {
 		bottom: 0;
-		left: 0;
+		right: 0;
 		background: rgb(255, 184, 183);
 	}
 
 	.panel-chart {
 		bottom: 0;
-		right: 0;
+		left: 0;
 		background: rgb(255, 255, 183);
 	}
 </style>
---
+++ src/components/ui2/AppMenu.svelte
@@ -0,0 +1,35 @@
+<div class="app-menu">
+	<ul>
+		<li>Search</li>
+		<li>User Guide</li>
+		<li>Download Datasets</li>
+	</ul>
+</div>
+
+<style>
+	.app-menu {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+
+		border: 2px greenyellow dashed;
+	}
+
+	.app-menu ul {
+		/* display: flex; */
+		list-style: none;
+		margin: 0;
+		padding: 0;
+		border: 1px red solid;
+	}
+
+	.app-menu li {
+		display: inline-block;
+		margin: 0;
+		padding: 0;
+		border: 1px blue dotted;
+
+	}
+</style>
