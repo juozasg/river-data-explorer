04e35a4c404315e423830acf6d89250b18e3a29e
Juozas Gaigalas <juozasgaigalas@gmail.com>
2025-02-17 23:46:05 +0200

    app menu wip

--- src/components/ui2/AppMenu.svelte
+++ src/components/ui2/AppMenu.svelte
@@ -5,6 +5,7 @@
 	const { mobile }: { mobile: boolean } = $props();
 
 	let open = $state(false);
+	let searchFocused = $state(false);
 </script>
 
 <div class="app-menu" class:mobile use:clickOutside={() => {open = false}}>
@@ -12,9 +13,11 @@
 		<HamburgerButton bind:open />
 	</div>
 	<ul class:open>
-		<li><button>Search</button></li>
-		<li><button>User Guide</button></li>
-		<li><button>Download Datasets</button></li>
+		<li class="search"><input type="text" placeholder="Search" bind:focused={searchFocused}/></li>
+		{#if !searchFocused}
+		<li><a target="_blank" href="help.html">User Guide</a></li>
+		<li><a target="_blank" href="https://github.com/Limnogirl90/SJRBC-web-map-data/tree/webapp/datasets">Download Datasets</a></li>
+		{/if}
 	</ul>
 </div>
 
@@ -32,19 +35,43 @@
 	}
 
 	ul {
-		/* display: flex; */
+		display: flex;
 		list-style: none;
 		margin: 0;
 		padding: 0;
 		border: 1px red solid;
+		/* display: inline-flex; */
 	}
 
 	li {
+		/* height: 40px; */
 		display: inline-block;
 		margin: 0;
 		padding: 0;
-		border: 1px blue dotted;
+		border: 2px blue dotted;
 		font-weight: bold;
+	}
+
+	li.search {
+		padding: 0;
+		margin: 0;
+		/* width: 42px; */
+		input {
+			width: 42px !important;
+			height: 36px;
+			padding: 0;
+			margin: 0;
+			/* position: absolute; */
+			/* top: 0; */
+			/* left: 0; */
+		}
+/*
+		input:hover, input:focus {
+			width: 100% !important;
+			position: absolute;
+			top: 0;
+			left: 0;
+		} */
 	}
 
 	.app-menu.mobile {
@@ -69,7 +96,7 @@
 				display: block;
 				width: 100%;
 
-				button {
+				a {
 					width: 100%;
 					padding: 1rem;
 					text-align: left;
