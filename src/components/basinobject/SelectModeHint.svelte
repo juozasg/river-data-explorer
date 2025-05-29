3d001ba939a4cd16385e69d318c6e0dd5db7d339
Juozas Gaigalas <juozasgaigalas@gmail.com>
2025-05-29 17:30:54 +0300

    animate selection hint

--- src/components/basinobject/SelectModeHint.svelte
+++ src/components/basinobject/SelectModeHint.svelte
@@ -7,6 +7,13 @@
 </script>
 
 <div class="select-mode-hint">
+	<div class="line"></div>
+	<div class="line"></div>
+	<div class="line"></div>
+	<div class="line"></div>
+	<div class="line"></div>
+	<div class="line"></div>
+
 	{#if mapSelectionMode.mode !== "auto"}
 		Click <span class="type-label">{typeLabel}</span> on the map to select
 	{:else}
@@ -14,20 +21,31 @@
 	{/if}
 </div>
 
-
 <style>
+
+	@keyframes border-pulsate {
+			0%   { border-color: rgba(61,156,190, 1); }
+			50%  { border-color: rgba(61,156,190, 0.25); }
+			100% { border-color: rgba(61,156,190, 1); }
+	}
+
+
 	.select-mode-hint {
-		margin: 6px;
-		margin-left: 0;
-		border: 2px dashed var(--stjoe-blue);
-		border-radius: 4px;
-		font-style: italic;
-		font-weight: 500;
-		padding: 4px;
-		width: fit-content;
 		.type-label {
 			font-weight: 600;
 			color: var(--stjoe-blue);
 		}
+
+		margin: 6px;
+		margin-left: 0;
+		border: 2.5px dotted var(--stjoe-blue);
+		border-radius: 4px;
+		transition: all 1s;
+		font-style: italic;
+		font-weight: 500;
+		padding: 4px;
+		width: fit-content;
+		animation: border-pulsate 1.25s infinite;
+
 	}
 </style>
